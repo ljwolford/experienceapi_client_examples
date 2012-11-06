91c055b925dde13d5816bd931a9e2d742e8e6205
ljwolford <lou.wolford.ctr@adlnet.gov>
2012-11-06 15:24:04 -0500

    updated location stmts-now getting location error

---
+++ 0.95/original_prototypes/config.js
@@ -0,0 +1,9 @@
+//globals: equal, responseText, statement, ok, deepEqual, QUnit, module, asyncTest, Util, start, golfStatements, console
+/*jslint bitwise: true, browser: true, plusplus: true, maxerr: 50, indent: 4 */
+function Config() {
+	"use strict";
+}
+Config.endpoint = "http://127.0.0.1:8000/";
+Config.authUser = "lou";
+Config.authPassword = "password";
+Config.actor = { "mbox":"lou.wolford.ctr@adlnet.gov", "name":"Lou Wolford" };
